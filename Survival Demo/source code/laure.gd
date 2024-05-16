eee9acea698a870f19de4ab9e1d64c049c794f2c
Jean-Milost Reymond <jean_milost@hotmail.com>
2024-05-16 21:25:21 +0200

    - added: message is now shown on player when he tries to open the first door - added: now cameras are switched when player is walking in the corridor - cleaned: code and comments

--- Survival Demo/source code/laure.gd
+++ Survival Demo/source code/laure.gd
@@ -1,9 +1,9 @@
 extends CharacterBody3D
 
-#const JUMP_VELOCITY = 4.5
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-#var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+# components
+@onready var g_Pivot      = $Pivot
+@onready var g_Animations = $AnimationTree
+@onready var g_WalkSound  = $Sounds/Walk
 
 # player constants
 const g_WalkingSpeed  = 1.5
@@ -22,7 +22,7 @@ func _physics_process(delta):
 
 	# move the player
 	if Input.is_action_pressed("move_forward"):
-		var direction = $Pivot.global_transform.basis.z.normalized()
+		var direction = g_Pivot.global_transform.basis.z.normalized()
 
 		velocity.x = direction.x * g_WalkingSpeed
 		velocity.z = direction.z * g_WalkingSpeed
@@ -30,40 +30,20 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0.0, g_WalkingSpeed)
 		velocity.z = move_toward(velocity.z, 0.0, g_WalkingSpeed)
 
-	# Add the gravity.
-	#if not is_on_floor():
-	#	velocity.y -= gravity * delta
-
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-	#	velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	#var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	#var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-
-	#if direction:
-	#	velocity.x = direction.x * SPEED
-	#	velocity.z = direction.z * SPEED
-	#else:
-	#	velocity.x = move_toward(velocity.x, 0, SPEED)
-	#	velocity.z = move_toward(velocity.z, 0, SPEED)
-
 	# get player state
 	var isIdle    = velocity == Vector3.ZERO
 	var isWalking = velocity != Vector3.ZERO
 	
 	# change the animation state depending on the user action
-	$AnimationTree.set("parameters/conditions/isIdle",    isIdle)
-	$AnimationTree.set("parameters/conditions/isWalking", isWalking)
+	g_Animations.set("parameters/conditions/isIdle",    isIdle)
+	g_Animations.set("parameters/conditions/isWalking", isWalking)
 	
 	# play the walk sound if player is walking
 	if isWalking:
-		if !$Sounds/Walk.is_playing():
-			$Sounds/Walk.play();
+		if !g_WalkSound.is_playing():
+			g_WalkSound.play();
 	else:
-		if $Sounds/Walk.is_playing():
-			$Sounds/Walk.stop();
-		
+		if g_WalkSound.is_playing():
+			g_WalkSound.stop();
+
 	move_and_slide()
--- Survival Demo/source code/main.gd
+++ Survival Demo/source code/main.gd
@@ -1,29 +1,43 @@
 extends Node
 
+# components
+@onready var g_Player           = $Scene/Laure
 @onready var g_Camera1          = $Scene/Cameras/Camera_1
 @onready var g_Camera2          = $Scene/Cameras/Camera_2
+@onready var g_Camera3          = $Scene/Cameras/Camera_3
 @onready var g_DoorCamera       = $Scene/Environment/Door/Camera_Pivot/Door_Camera
 @onready var g_DoorPlayer       = $Scene/Environment/Door/Door_Pivot/AnimationPlayer
 @onready var g_DoorCameraPlayer = $Scene/Environment/Door/Camera_Pivot/AnimationPlayer
+@onready var g_Label            = $User_Interface/Label
+@onready var g_DoorHandleSound  = $Sounds/Door_Handle_Turns
 
-var g_InArea1 = true
-var g_InArea2 = false
+# flags
+var g_PlayerHitDoor1 = false
+var g_InArea1        = true
+var g_InArea2        = false
+var g_InArea3        = false
+
+# times
+var g_MsgShowTime   = 2000
+var g_MsgTrialStamp = 0
 
 # Called when the node enters the scene tree for the first time
 func _ready():
 	g_Camera1.current = true
+	g_Label.visible   = false
 
 ###
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #@param delta - elapsed time in seconds since the previous call
 ##
 func _process(delta):
-	#if g_Camera1:
-		#g_Camera1.current = true
+	if g_PlayerHitDoor1:
+		g_MsgTrialStamp  = Time.get_ticks_msec()
+		g_PlayerHitDoor1 = false
 
-	#if $Scene/Environment/Door/Camera_Pivot/Door_Camera:
-		#$Scene/Environment/Door/Camera_Pivot/Door_Camera.current = true
-
+	if (Time.get_ticks_msec() >= g_MsgTrialStamp + g_MsgShowTime):
+		g_Label.visible = false
+	
 	if Input.is_action_pressed("player_action"):
 		RunDoorAnim()
 
@@ -38,7 +52,7 @@ func RunDoorAnim():
 	if !g_DoorPlayer.is_playing():
 		g_DoorPlayer.play("door_opening")
 	
-	#start the camera animation
+	# start the camera animation
 	if !g_DoorCameraPlayer.is_playing():
 		g_DoorCameraPlayer.play("camera_walking")
 
@@ -47,10 +61,20 @@ func RunDoorAnim():
 #@param body - body which entered in the area
 ##
 func _on_trigger_1_body_entered(body):
+	if body != g_Player:
+		return
+
 	g_Camera1.current = true
 	g_InArea1         = true
 
+###
+# Called when a body exited the first trigger area
+#@param body - body which exited the area
+##
 func _on_trigger_1_body_exited(body):
+	if body != g_Player:
+		return
+
 	# quitting area 1 but still in area 2?
 	if g_InArea2:
 		g_Camera2.current = true
@@ -62,12 +86,66 @@ func _on_trigger_1_body_exited(body):
 #@param body - body which entered in the area
 ##
 func _on_trigger_2_body_entered(body):
+	if body != g_Player:
+		return
+
 	g_Camera2.current = true
 	g_InArea2         = true
 
+###
+# Called when a body exited the second trigger area
+#@param body - body which exited the area
+##
 func _on_trigger_2_body_exited(body):
+	if body != g_Player:
+		return
+
 	# quitting area 2 but still in area 1?
 	if g_InArea1:
 		g_Camera1.current = true
+	# quitting area 2 but still in area 1?
+	elif g_InArea3:
+		g_Camera3.current = true
 
 	g_InArea2 = false
+
+###
+# Called when a body entered on the third trigger area
+#@param body - body which entered in the area
+##
+func _on_trigger_3_body_entered(body):
+	if body != g_Player:
+		return
+
+	g_Camera3.current = true
+	g_InArea3         = true
+
+###
+# Called when a body exited the third trigger area
+#@param body - body which exited the area
+##
+func _on_trigger_3_body_exited(body):
+	if body != g_Player:
+		return
+
+	# quitting area 3 but still in area 2?
+	if g_InArea2:
+		g_Camera2.current = true
+
+	g_InArea3 = false
+
+###
+# Called when a body entered on the fourth trigger area
+#@param body - body which entered in the area
+##
+func _on_trigger_4_body_entered(body):
+	if body != g_Player:
+		return
+
+	if !g_DoorHandleSound.is_playing():
+		g_DoorHandleSound.play();
+
+	# show a message to user about the door status
+	g_Label.text     = "The door is closed and won't open."
+	g_Label.visible  = true
+	g_PlayerHitDoor1 = true
