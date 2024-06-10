2470b08828d4e23621072c97754d15abbf077473
Jean-Milost Reymond <jean_milost@hotmail.com>
2024-06-10 22:05:07 +0200

    - added: zombie model (NOT TERMINATED) - added: now Laure can fire (NOT TERMINATED) - changed: turned models local

--- Survival Demo/source code/laure.gd
+++ Survival Demo/source code/laure.gd
@@ -25,6 +25,9 @@ func _physics_process(delta):
 
 		return
 
+	# check if player is firing
+	var isFiring = Input.is_action_pressed("fire_modifier_action")
+
 	# rotate the player
 	if Input.is_action_pressed("turn_right"):
 		rotation.y -= delta * g_RotationSpeed 
@@ -32,7 +35,7 @@ func _physics_process(delta):
 		rotation.y += delta * g_RotationSpeed
 
 	# move the player
-	if Input.is_action_pressed("move_forward"):
+	if Input.is_action_pressed("move_forward") && !isFiring:
 		var direction = g_Pivot.global_transform.basis.z.normalized()
 
 		velocity.x = direction.x * g_WalkingSpeed
@@ -42,12 +45,13 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0.0, g_WalkingSpeed)
 
 	# get player state
-	var isIdle    = velocity == Vector3.ZERO
-	var isWalking = velocity != Vector3.ZERO
+	var isIdle    = velocity == Vector3.ZERO && !isFiring
+	var isWalking = velocity != Vector3.ZERO && !isFiring
 	
 	# change the animation state depending on the user action
 	g_Animations.set("parameters/conditions/isIdle",    isIdle)
 	g_Animations.set("parameters/conditions/isWalking", isWalking)
+	g_Animations.set("parameters/conditions/isFiring",  isFiring)
 	
 	# play the walk sound if player is walking
 	if isWalking:
@@ -65,5 +69,17 @@ func _physics_process(delta):
 func _on_main_on_door_opening():
 	g_DoorOpening = true
 
+###
+# Called when laboratory door animation finished
+##
 func _on_environment_on_door_anim_finished():
 	g_DoorOpening = false
+
+###
+# Called when character animation finished
+#@param anim_name - animation name which just finished
+##
+func _on_animation_tree_animation_finished(anim_name):
+	# ignore all animation but the fire one
+	if anim_name != "fire":
+		return
