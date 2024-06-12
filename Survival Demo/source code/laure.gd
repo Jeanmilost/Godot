2c6b68149b9c6e499cf2060bb7398fc9c98e6b3c
Jean-Milost Reymond <jean_milost@hotmail.com>
2024-06-12 21:19:40 +0200

    - added: simple state machine - added: running and firing states for laure - changed: updated (and simplified) Laure model

--- Survival Demo/source code/laure.gd
+++ Survival Demo/source code/laure.gd
@@ -1,16 +1,32 @@
 extends CharacterBody3D
 
+const StateMachine = preload("res://source code/state_machine.gd")
+
 # components
 @onready var g_Pivot      = $Pivot
 @onready var g_Animations = $AnimationTree
 @onready var g_WalkSound  = $Sounds/Walk
+@onready var g_RunSound   = $Sounds/Run
+@onready var g_FireSound  = $Sounds/Fire
+
+# game objects
+var g_StateMachine = null
 
 # player constants
 const g_WalkingSpeed  = 1.5
+const g_RunningSpeed  = 3
 const g_RotationSpeed = 4
 
 # flags
-var g_DoorOpening = false
+var g_DoorOpening     = false
+var g_IsFiring        = false
+var g_FireSoundPlayed = false
+
+###
+# Called when the node enters the scene tree for the first time
+##
+func _ready():
+	g_StateMachine = StateMachine.new(g_Animations)
 
 ###
 # Called every frame at a fixed rate, which allows any processing that requires the physics values
@@ -25,8 +41,11 @@ func _physics_process(delta):
 
 		return
 
-	# check if player is firing
-	var isFiring = Input.is_action_pressed("fire_modifier_action")
+	# check if player is running
+	var isRunning = Input.is_action_pressed("run_modifier_action")
+
+	# check if player is pointing his gun
+	var isPointingGun = velocity == Vector3.ZERO && Input.is_action_pressed("fire_modifier_action")
 
 	# rotate the player
 	if Input.is_action_pressed("turn_right"):
@@ -35,31 +54,84 @@ func _physics_process(delta):
 		rotation.y += delta * g_RotationSpeed
 
 	# move the player
-	if Input.is_action_pressed("move_forward") && !isFiring:
+	if Input.is_action_pressed("move_forward") && !isPointingGun:
 		var direction = g_Pivot.global_transform.basis.z.normalized()
 
-		velocity.x = direction.x * g_WalkingSpeed
-		velocity.z = direction.z * g_WalkingSpeed
+		if isRunning:
+			velocity.x = direction.x * g_RunningSpeed
+			velocity.z = direction.z * g_RunningSpeed
+		else:
+			velocity.x = direction.x * g_WalkingSpeed
+			velocity.z = direction.z * g_WalkingSpeed
 	else:
-		velocity.x = move_toward(velocity.x, 0.0, g_WalkingSpeed)
-		velocity.z = move_toward(velocity.z, 0.0, g_WalkingSpeed)
+		if isRunning:
+			velocity.x = move_toward(velocity.x, 0.0, g_RunningSpeed)
+			velocity.z = move_toward(velocity.z, 0.0, g_RunningSpeed)
+		else:
+			velocity.x = move_toward(velocity.x, 0.0, g_WalkingSpeed)
+			velocity.z = move_toward(velocity.z, 0.0, g_WalkingSpeed)
 
 	# get player state
-	var isIdle    = velocity == Vector3.ZERO && !isFiring
-	var isWalking = velocity != Vector3.ZERO && !isFiring
-	
+	var isIdle    = velocity == Vector3.ZERO && !isPointingGun
+	var isWalking = velocity != Vector3.ZERO && !isPointingGun
+
+	if !isPointingGun:
+		g_IsFiring        = false
+		g_FireSoundPlayed = false
+
+	if !g_IsFiring:
+		g_IsFiring = Input.is_action_pressed("fire")
+
 	# change the animation state depending on the user action
-	g_Animations.set("parameters/conditions/isIdle",    isIdle)
-	g_Animations.set("parameters/conditions/isWalking", isWalking)
-	g_Animations.set("parameters/conditions/isFiring",  isFiring)
-	
-	# play the walk sound if player is walking
+	if isIdle:
+		g_StateMachine._set_state(StateMachine.IEState.S_Idle)
+	elif isWalking:
+		if isRunning:
+			g_StateMachine._set_state(StateMachine.IEState.S_Run)
+		else:
+			g_StateMachine._set_state(StateMachine.IEState.S_Walk)
+	elif isPointingGun:
+		if g_IsFiring:
+			g_StateMachine._set_state(StateMachine.IEState.S_Fire)
+		else:
+			g_StateMachine._set_state(StateMachine.IEState.S_Fire_Idle)
+
+	# apply the state machine
+	g_StateMachine.run()
+
+	# play the walk or run sound if player is walking or running
 	if isWalking:
-		if !g_WalkSound.is_playing():
-			g_WalkSound.play();
+		if isRunning:
+			if g_WalkSound.is_playing():
+				g_WalkSound.stop();
+
+			if !g_RunSound.is_playing():
+				g_RunSound.play();
+		else:
+			if g_RunSound.is_playing():
+				g_RunSound.stop();
+
+			if !g_WalkSound.is_playing():
+				g_WalkSound.play();
 	else:
 		if g_WalkSound.is_playing():
 			g_WalkSound.stop();
+
+		if g_RunSound.is_playing():
+			g_RunSound.stop();
+
+	# play the gun fire sound
+	if isPointingGun && g_IsFiring && !g_FireSoundPlayed:
+		if g_WalkSound.is_playing():
+			g_WalkSound.stop();
+
+		if g_RunSound.is_playing():
+			g_RunSound.stop();
+
+		if !g_FireSound.is_playing():
+			g_FireSound.play();
+
+		g_FireSoundPlayed = true
 
 	move_and_slide()
 
@@ -83,3 +155,8 @@ func _on_animation_tree_animation_finished(anim_name):
 	# ignore all animation but the fire one
 	if anim_name != "fire":
 		return
+
+	g_IsFiring        = false
+	g_FireSoundPlayed = false
+
+	g_StateMachine._set_state(StateMachine.IEState.S_Fire_Idle)
---
+++ Survival Demo/source code/state_machine.gd
@@ -0,0 +1,131 @@
+###
+# https://gdscript.com/tutorials/classes/
+##
+class_name StateMachine
+
+#states
+enum IEState {S_Idle, S_Walk, S_Run, S_Fire_Idle, S_Fire, S_Repulse, S_Hit, S_Die}
+
+# properties
+var m_Animations = null
+var m_State      = IEState.S_Idle
+
+###
+# Constructor
+#@param animations - animations used by the state machine
+##
+func _init(animations):
+	m_Animations = animations
+
+###
+# Gets the current machine state
+#@return current machine state
+##
+func _get_state():
+	return m_State
+
+###
+# Sets the current machine state
+#@param state - current machine state
+##
+func _set_state(state):
+	if state != m_State:
+		m_Animations.active = false
+		m_Animations.active = true
+
+	m_State = state
+
+###
+# Runs the state machine
+##
+func run():
+	match m_State:
+		IEState.S_Idle:
+			m_Animations.set("parameters/conditions/isIdle", true)
+			m_Animations.set("parameters/conditions/isWalking", false)
+			m_Animations.set("parameters/conditions/isRunning", false)
+			m_Animations.set("parameters/conditions/isFireIdle", false)
+			m_Animations.set("parameters/conditions/isFiring", false)
+			m_Animations.set("parameters/conditions/isRepulsing", false)
+			m_Animations.set("parameters/conditions/isHit", false)
+			m_Animations.set("parameters/conditions/isDying", false)
+
+		IEState.S_Walk:
+			m_Animations.set("parameters/conditions/isIdle", false)
+			m_Animations.set("parameters/conditions/isWalking", true)
+			m_Animations.set("parameters/conditions/isRunning", false)
+			m_Animations.set("parameters/conditions/isFireIdle", false)
+			m_Animations.set("parameters/conditions/isFiring", false)
+			m_Animations.set("parameters/conditions/isRepulsing", false)
+			m_Animations.set("parameters/conditions/isHit", false)
+			m_Animations.set("parameters/conditions/isDying", false)
+
+		IEState.S_Run:
+			m_Animations.set("parameters/conditions/isIdle", false)
+			m_Animations.set("parameters/conditions/isWalking", false)
+			m_Animations.set("parameters/conditions/isRunning", true)
+			m_Animations.set("parameters/conditions/isFireIdle", false)
+			m_Animations.set("parameters/conditions/isFiring", false)
+			m_Animations.set("parameters/conditions/isRepulsing", false)
+			m_Animations.set("parameters/conditions/isHit", false)
+			m_Animations.set("parameters/conditions/isDying", false)
+
+		IEState.S_Fire_Idle:
+			m_Animations.set("parameters/conditions/isIdle", false)
+			m_Animations.set("parameters/conditions/isWalking", false)
+			m_Animations.set("parameters/conditions/isRunning", false)
+			m_Animations.set("parameters/conditions/isFireIdle", true)
+			m_Animations.set("parameters/conditions/isFiring", false)
+			m_Animations.set("parameters/conditions/isRepulsing", false)
+			m_Animations.set("parameters/conditions/isHit", false)
+			m_Animations.set("parameters/conditions/isDying", false)
+
+		IEState.S_Fire:
+			m_Animations.set("parameters/conditions/isIdle", false)
+			m_Animations.set("parameters/conditions/isWalking", false)
+			m_Animations.set("parameters/conditions/isRunning", false)
+			m_Animations.set("parameters/conditions/isFireIdle", false)
+			m_Animations.set("parameters/conditions/isFiring", true)
+			m_Animations.set("parameters/conditions/isRepulsing", false)
+			m_Animations.set("parameters/conditions/isHit", false)
+			m_Animations.set("parameters/conditions/isDying", false)
+
+		IEState.S_Repulse:
+			m_Animations.set("parameters/conditions/isIdle", false)
+			m_Animations.set("parameters/conditions/isWalking", false)
+			m_Animations.set("parameters/conditions/isRunning", false)
+			m_Animations.set("parameters/conditions/isFireIdle", false)
+			m_Animations.set("parameters/conditions/isFiring", false)
+			m_Animations.set("parameters/conditions/isRepulsing", true)
+			m_Animations.set("parameters/conditions/isHit", false)
+			m_Animations.set("parameters/conditions/isDying", false)
+
+		IEState.S_Hit:
+			m_Animations.set("parameters/conditions/isIdle", false)
+			m_Animations.set("parameters/conditions/isWalking", false)
+			m_Animations.set("parameters/conditions/isRunning", false)
+			m_Animations.set("parameters/conditions/isFireIdle", false)
+			m_Animations.set("parameters/conditions/isFiring", false)
+			m_Animations.set("parameters/conditions/isRepulsing", false)
+			m_Animations.set("parameters/conditions/isHit", true)
+			m_Animations.set("parameters/conditions/isDying", false)
+
+		IEState.S_Die:
+			m_Animations.set("parameters/conditions/isIdle", false)
+			m_Animations.set("parameters/conditions/isWalking", false)
+			m_Animations.set("parameters/conditions/isRunning", false)
+			m_Animations.set("parameters/conditions/isFireIdle", false)
+			m_Animations.set("parameters/conditions/isFiring", false)
+			m_Animations.set("parameters/conditions/isRepulsing", false)
+			m_Animations.set("parameters/conditions/isHit", false)
+			m_Animations.set("parameters/conditions/isDying", true)
+
+		_:
+			m_Animations.set("parameters/conditions/isIdle", false)
+			m_Animations.set("parameters/conditions/isWalking", false)
+			m_Animations.set("parameters/conditions/isRunning", false)
+			m_Animations.set("parameters/conditions/isFireIdle", false)
+			m_Animations.set("parameters/conditions/isFiring", false)
+			m_Animations.set("parameters/conditions/isRepulsing", false)
+			m_Animations.set("parameters/conditions/isHit", false)
+			m_Animations.set("parameters/conditions/isDying", false)
