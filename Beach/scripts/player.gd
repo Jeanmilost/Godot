d069c70f8d6068a313bd146d86cafdbf400ea5da
Jean-Milost Reymond <jean_milost@hotmail.com>
2024-09-27 22:03:18 +0200

    - added: player to beach scene - improved: coast collider on beach scene - improved: environment rendering on beach scene

--- Beach/scripts/player.gd
+++ Beach/scripts/player.gd
@@ -1,31 +1,32 @@
 extends CharacterBody3D
 
+# player constants
+const g_WalkingSpeed  = 1.5
+const g_RotationSpeed = 4
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-
-
+###
+# Called every frame at a fixed rate, which allows any processing that requires the physics values
+#@param delta - elapsed time in seconds since the previous call
+##
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
+	# rotate the player
+	if Input.is_action_pressed("turn_right"):
+		rotation.y -= delta * g_RotationSpeed
+	elif Input.is_action_pressed("turn_left"):
+		rotation.y += delta * g_RotationSpeed
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	# move the player
+	if Input.is_action_pressed("move_forward"):
+		var direction = $Pivot.global_transform.basis.z.normalized()
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * g_WalkingSpeed
+		velocity.z = direction.z * g_WalkingSpeed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0.0, g_WalkingSpeed)
+		velocity.z = move_toward(velocity.z, 0.0, g_WalkingSpeed)
 
+	# change the animation state depending on the user action
+	$AnimationTree.set("parameters/conditions/isIdle",    velocity == Vector3.ZERO)
+	$AnimationTree.set("parameters/conditions/isWalking", velocity != Vector3.ZERO)
+	
 	move_and_slide()
