4567b9d57bc6c0088005ac8bf853264cdfff07ef
Jean-Milost Reymond <jean_milost@hotmail.com>
2025-05-12 20:36:29 +0200

    - added: ancient dome in main scene - cleaned: code and comments

--- Shoot'em'up Demo/scripts/camera.gd
+++ Shoot'em'up Demo/scripts/camera.gd
@@ -1,12 +1,9 @@
 extends Camera3D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+###
+# Called every frame
+#@param delta - elapsed time in seconds since the previous frame
+##
 func _process(delta):
 	# horizontal scroll
 	position.z += -10 * delta;
